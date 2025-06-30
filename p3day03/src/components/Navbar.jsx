573c10708ddcd9c9fd666abe09b567da0da25d83
Vignesh S <s.vnesh@gmail.com>
2025-06-30 22:53:40 +0530

    Add files via upload

--- p3day03/src/components/Navbar.jsx
+++ p3day03/src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+  const { isLoggedIn, isLogOut } = useAuth();
+
+  const handleLogout = () => {
+    isLogOut();
+    navigate('/login');
+  }
+
   return (
-    <nav className="flex gap-4 p-3 bg-gray-300 text-white">
+    <nav className="flex gap-6 p-3 bg-gray-600 text-white items-center justify-end">
       <NavLink to="/home">Home</NavLink>
-      <NavLink to="/About">About</NavLink>
+      <NavLink to="/about">About</NavLink>
       <NavLink to="/product">Product</NavLink>
+      <button onClick={handleLogout} className={ isLoggedIn ? "p-2 bg-blue-400 rounded-2xl items-center w-30 cursor-pointer" : ""}>{isLoggedIn ? "Logout" : ""}</button>
     </nav>
   )
 }
--- p3day03/src/components/ProtectedRoute.jsx
+++ p3day03/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
 
-
-
+  const { isLoggedIn } = useAuth();
 
   return (
-    <></>
+    isLoggedIn ? children : <Navigate to='/login' replace /> 
   )
 }
 
